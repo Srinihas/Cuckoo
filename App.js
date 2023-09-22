42898f4ee4bbc9ba8b812f34027e746fffff7340
Srinihas <115796290+Srinihas@users.noreply.github.com>
2023-09-22 15:42:29 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
+import Events from './screens/Events';
+import Alarm from './screens/Alarm';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 
 export default function App() {
   return (
-    <Home />
+    <AppContainer />
   );
 }
+
+const AppNavigator = createSwitchNavigator({
+  Home: Home,
+  Events: Events,
+  Alarms: Alarm
+})
+
+const AppContainer = createAppContainer(AppNavigator)
 
 const styles = StyleSheet.create({
   container: {
