db1ba7935237fe2aac5bc4f677785eeba97daf1e
Srinihas <115796290+Srinihas@users.noreply.github.com>
2023-09-22 15:40:51 +0400

    Update Events.js

--- screens/Events.js
+++ screens/Events.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Flatlist} from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, Flatlist, SafeAreaView} from 'react-native';
 
 export default class Events extends React.Component {
   render(){
     return (
-      <View>
-        <Text> Events Screen </Text>
+      <View style={styles.container}>
+      <Text style={styles.titleText}> Events Screen </Text>
       <TouchableOpacity onPress={()=>{
-          this.props.navigation.navigate("Home");
-        }}>
-          <Text style={styles.text}> Go Back </Text>
-        </TouchableOpacity>
-      </View>
+        this.props.navigation.navigate("Home");
+      }}>
+        <Text style={styles.text}> Go Back </Text>
+      </TouchableOpacity>
+    </View>
     );
   }
 }
@@ -22,5 +22,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'black'
   },
+  titleText: {
+    fontWeight: "bold",
+    fontSize: 50,
+    padding: 20,
+    color: "white",
+    alignSelf: 'center'
+  },
+  text:{
+    color:'white', 
+    fontWeight:'bold', 
+    padding: 20, 
+    alignSelf:'center',
+    fontSize: 20
+  }
 });
